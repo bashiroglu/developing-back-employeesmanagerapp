059afb8c7caf87b06b25ebb190750d6eb3813b6d
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2020-01-12 18:36:18 +0100

    added headers to app js to avoid CORS error

--- App.js
+++ App.js
@@ -5,6 +5,16 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept '
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
+
 app.use(parser.json());
 app.use('/api/v1/users', userRoutes);
 
